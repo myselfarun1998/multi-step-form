2eaffaae84361bb73883161fc54b65c075b83fa9
Arun kumar <72678258+myselfarun1998@users.noreply.github.com>
2022-01-19 09:26:07 +0530

    Add files via upload

---
+++ src/Component/SecondStep.js
@@ -0,0 +1,81 @@
+import React, { useState, useContext } from 'react';
+
+import { Button, TextField } from '@material-ui/core';
+import { RadioGroup } from '@mui/material';
+import { Radio } from '@mui/material';
+import { FormControlLabel, makeStyles } from '@mui/material';
+import { FormLabel } from '@mui/material';
+import { FormControl } from '@mui/material';
+import InputAdornment from '@mui/material/InputAdornment';
+import { multiStepContext } from '../StepContext';
+
+const SecondStep = () => {
+  const [course, setcourse] = useState('java');
+  const { setStep, userData, setUserData } = useContext(multiStepContext);
+
+  return (
+    <div>
+      <label>enter the collage name</label>
+      <br />
+      <input
+        type="text"
+        value={userData['cname']}
+        onChange={(e) => setUserData({ ...userData, cname: e.target.value })}
+      />
+
+      <div className="radio-btn-container">
+        <div className="radio-btn">
+          <input
+            type="radio"
+            name="course"
+            checked={userData.course === 'java'}
+            onChange={() =>
+              setUserData((prev) => ({ ...prev, course: 'java' }))
+            }
+          />
+          java
+        </div>
+
+        <div className="radio-btn">
+          <input
+            type="radio"
+            name="course"
+            checked={userData.course === 'c++'}
+            onChange={() => setUserData((prev) => ({ ...prev, course: 'c++' }))}
+          />
+          c++
+        </div>
+        <div className="radio-btn">
+          <input
+            type="radio"
+            name="course"
+            checked={userData.course === 'ASP'}
+            onChange={() => setUserData((prev) => ({ ...prev, course: 'ASP' }))}
+          />
+          ASP
+        </div>
+      </div>
+      <label> your selected course is</label>
+      <br />
+      <input type="text" value={userData.course} />
+
+      <div>
+        <button
+          variant="contained"
+          onClick={() => setStep(1)}
+          color="secondary"
+        >
+          {' '}
+          previous{' '}
+        </button>
+        <button variant="contained" onClick={() => setStep(3)} color="primary">
+          {' '}
+          Next{' '}
+        </button>{' '}
+        <span></span>
+      </div>
+    </div>
+  );
+};
+
+export default SecondStep;
